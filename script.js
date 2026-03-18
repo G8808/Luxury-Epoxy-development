f5d26d46623d2ce4a36cbd6c177e6d6edfa58aae
G8808 <matosgiancarlo8@gmail.com>
2026-03-18 14:06:12 +0000

    fd

---
+++ script.js
@@ -0,0 +1,22 @@
+const { createApp, ref } = Vue;
+
+createApp({
+  // Data / State
+  data() {
+    return {
+     
+      
+ 
+    }
+  },
+
+  // Functions / Methods
+  methods: {
+   
+  },
+
+  // Runs when the app starts
+  mounted() {
+    console.log("Vue is active and polished.");
+  }
+}).mount('#app');
